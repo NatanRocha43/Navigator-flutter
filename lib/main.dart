b3d9434555dd2fc8134fa25528721a4ad81af2fe
NATAN DE PAULA ROCHA <3039331@itp.local>
2025-05-05 21:54:12 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(title: 'Navigation Basics', home: FirstRoute()));
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Primeiro'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Tela principal'), 
+          automaticallyImplyLeading: false
+        ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Cadastrar'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ThirdRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+class ThirdRoute extends StatelessWidget {
+  const ThirdRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Cadastro'),
+          automaticallyImplyLeading: false 
+        ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Salvar'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const FourthRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+class FourthRoute extends StatelessWidget {
+  const FourthRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('FIM'),
+          automaticallyImplyLeading: false 
+        ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => FirstRoute()),
+              (Route<dynamic> route) => false,
+            );
+          },
+          child: const Text('Voltar ao inicio'),
+        ),
+        
+      ),
+    );
+  }
+}
